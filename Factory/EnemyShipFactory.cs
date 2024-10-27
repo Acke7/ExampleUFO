0418d38bf26c89e650b9bb14d30c2cd4bcae8715
acke7 <ahmadaloalbi@ouylook.com>
2024-10-27 16:26:43 +0100

    Add project files.

---
+++ Factory/EnemyShipFactory.cs
@@ -0,0 +1,26 @@
+﻿using ExampleUFO.Ships;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleUFO.Factory
+{
+    class EnemyShipFactory
+    {
+        public IEnemyShip MakeEnemyShip(string shipTypeChosen)
+        {
+            if (shipTypeChosen == "U")
+                return new UFOEnemyShip();
+            else if (shipTypeChosen == "R")
+                return new RocketEnemyShip();
+            else if (shipTypeChosen == "B")
+                return new BigUFOEnemyShip();
+            else if (shipTypeChosen == "BR")
+                return new BigRocketEnemyShip();
+            else
+                return null;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using ExampleUFO.Factory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleUFO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose a kind of enemy.");
+            Console.WriteLine("U: UFO");
+            Console.WriteLine("R: Rocket");
+            Console.WriteLine("B: Big UFO");
+            Console.WriteLine("BR: Big Rocket");
+            var ship = Console.ReadLine().ToUpper();
+
+            var myEnemyShipFactory =
+               new EnemyShipFactory();
+            var theEnemy =
+               myEnemyShipFactory.MakeEnemyShip(ship);
+
+            theEnemy.DisplayEnemyShip();
+            theEnemy.FollowHeroShip();
+            theEnemy.EnemyShipShoots();
+            
+            // Damage ship
+            theEnemy.ShipTakesDamage(10);
+            // Destroy Small ship
+            //theEnemy.ShipTakesDamage(101);
+            
+            Console.ReadLine();
+        }
+    }
+}
